757e40c4702e9e3d7e3fcb4fe9aa16fdfd51b8c7
aroedata <aroedata@gmail.com>
2019-09-04 18:33:05 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -9,7 +9,7 @@
         <meta name="author" content="">
         <title>SB Admin 2 - Login</title>
         <!-- Custom fonts for this template-->
-        <link href="<?= base_url('assets/') ?> vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
         <!-- Custom styles for this template-->
         <link href="<?= base_url('assets/') ?>css/sb-admin-2.min.css" rel="stylesheet">
@@ -25,21 +25,20 @@
                             <h3>Sign In</h3>
                         </div>
                         <div class="card-body">
-                            <form>
+                            <form method="post" action="<?= base_url('auth') ?>">
                                 <div class="input-group form-group">
                                     <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                        <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
                                     </div>
-                                    <input type="text" class="form-control" placeholder="username">
+                                    <input type="text" name="userid" autocomplete="off" class="form-control" placeholder="username">
 
                                 </div>
                                 <div class="input-group form-group">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-key"></i></span>
                                     </div>
-                                    <input type="password" class="form-control" placeholder="password">
+                                    <input type="password" name="password" autocomplete="off" class="form-control" placeholder="password">
                                 </div>
-
                                 <div class="form-group">
                                     <input type="submit" value="Login" class="btn float-right login_btn">
                                 </div>
