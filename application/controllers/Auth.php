ed49437868b5c0ba445c547c5416bd7d5fc472b6
aroedata <aroedata@gmail.com>
2019-10-20 17:10:24 +0700

    Setting Awal
    Setting awal

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,16 +3,50 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
     public function index()
     {
+        if ($this->session->userdata('email')) {
+            redirect('dashboard');
+        }
         $this->load->view('auth/header');
         $this->load->view('auth/login');
         $this->load->view('auth/footer');
     }
+    private function _login()
+    { }
     public function register()
     {
-        $this->load->view('auth/header');
-        $this->load->view('auth/register');
-        $this->load->view('auth/footer');
+        // if ($this->session->userdata('email')) {
+        //     redirect('dashboard');
+        // }
+        $this->form_validation->set_rules('nama', 'Nama', 'required|trim', [
+            'required' => 'Nama harap diisi!'
+        ]);
+        $this->form_validation->set_rules('username', 'Username', 'required|trim', [
+            'required' => 'Username harap diisi!'
+        ]);
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[users.email]', [
+            'required' => 'Email harap diisi!',
+            'valid_email' => 'Email tidak Valid!',
+            'is_unique' => 'Alamat email telah Digunakan!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'required' => 'Password harap diisi!',
+            'matches' => 'Password tidak sama!',
+            'min_length' => 'Password terlalu pendek!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+        if ($this->form_validation->run() == false) {
+            $this->load->view('Auth/header');
+            $this->load->view('Auth/register');
+            $this->load->view('Auth/footer');
+        } else {
+            echo "Oke";
+        }
     }
 }
--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -1,44 +1,55 @@
 <div class="container">
     <div class="d-flex justify-content-center h-100">
-        <div class="col-lg-7">
+        <div class="col-lg-7 align-items-center h-100">
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body">
                     <h3>Sign Up</h3>
                 </div>
                 <div class="card-body">
+                    <?= $this->session->flashdata('message'); ?>
                     <form method="post" action="<?= base_url('auth/register') ?>">
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
+                        <div class="form-group row">
+                            <label for="nama" class="col-sm-3 col-form-label">Nama</label>
+                            <div class="col">
+                                <input type="text" name="nama" id="nama" class="form-control" placeholder="Masukkan nama anda" autocomplete="off" value="<?= set_value('nama');  ?>">
+                                <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
-                            <input type="text" name="nama" autocomplete="off" class="form-control" placeholder="Nama">
                         </div>
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
+                        <div class="form-group row">
+                            <label for="username" class="col-sm-3 col-form-label">Username</label>
+                            <div class="col">
+                                <input type="text" name="username" id="username" class="form-control" placeholder="Masukkan username anda" autocomplete="off" value="<?= set_value('username');  ?>">
+                                <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
-                            <input type="text" name="userid" autocomplete="off" class="form-control" placeholder="username">
                         </div>
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-3 col-form-label">Email</label>
+                            <div class="col">
+                                <input type="text" name="email" id="email" class="form-control" placeholder="Masukkan email anda" autocomplete="off" value="<?= set_value('email');  ?>">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
-                            <input type="text" name="email" autocomplete="off" class="form-control" placeholder="email">
                         </div>
-                        <div class="input-group form-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        <div class="form-group row">
+                            <label for="pasword1" class="col-sm-3 col-form-label">Password</label>
+                            <div class="col">
+                                <input type="password" name="password1" id="password1" class="form-control" placeholder="Masukkan password" autocomplete="off">
+                                <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
-                            <input type="password" name="password" autocomplete="off" class="form-control" placeholder="password">
                         </div>
-                        <div class="form-group">
-                            <input type="submit" value="Login" class="btn float-right login_btn">
+                        <div class="form-group row">
+                            <label for="password2" class="col-sm-3 col-form-label">Ulangi Password</label>
+                            <div class="col">
+                                <input type="password" name="password2" id="password2" class="form-control" placeholder="Ulangi password" autocomplete="off">
+                            </div>
                         </div>
+                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                            Register Account
+                        </button>
                     </form>
                 </div>
                 <div class="card-body">
                     <div class="d-flex justify-content-center links">
-                        Telah memiliki akun?<a href="<?= base_url('Auth') ?>">Sign In</a>
+                        Telah memiliki akun? <a href="<?= base_url('Auth') ?>"> Sign In</a>
                     </div>
                 </div>
             </div>
